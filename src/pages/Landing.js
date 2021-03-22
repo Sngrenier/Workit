b66c942b0efe382c37637babb038cdd8288065bc
Stacy Grenier <sngrenier@gmail.com>
2021-03-22 11:29:19 -0700

    page change

---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+    render() {
+        return (
+            <div>
+                I'm the landing Component
+            </div>
+        )
+    }
+}
