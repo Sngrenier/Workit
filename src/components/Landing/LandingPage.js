9f928a87f287c955a40d186ae8934f9bf8f5e978
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-03-24 21:02:42 -0600

    removed authcontext from register component, added Paypal Btn and Profile component

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react'
 import CircuitList from './CircuitList'
 import Circuit from './Circuit'
-import NavButton from '../NavButton'
+import NavButton, { ButtonContainer } from '../NavButton'
+import axios from 'axios'
 
 
+const LandingPage=()=>{
 
-export default class LandingPage extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>WORKit</h1>
-
-                <h3>Circuits</h3>
-                <section className='circuitListCard'>
-                
-                <CircuitList/>
-                <Circuit/>
-
-                </section>
-
-                <footer className='navBar'>
-                Workouts
-                Planner
-                progress
-                food
-                </footer>
-          
-            
-            </div>
-        )
-    }
+const logout=()=>{
+    axios.post(``)
 }
+
+    return (
+        <div>
+            <h1>WORKit</h1>
+    
+            <h3>Circuits</h3>
+            <ButtonContainer >Logout</ButtonContainer>
+            <section className='circuitListCard'>
+            
+            <CircuitList/>
+            <Circuit/>
+    
+            </section>
+    
+            <footer className='navBar'>
+            Workouts
+            Planner
+            progress
+            food
+            </footer>
+      
+        
+        </div>
+    )
+    
+}
+export default LandingPage
 
 
 
