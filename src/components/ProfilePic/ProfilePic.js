e5493fac442477b75d0588e3cdc3a72b1a392c6d
Stacy Grenier <sngrenier@gmail.com>
2021-03-30 16:08:52 -0700

    profile-pic-updates

--- src/components/ProfilePic/ProfilePic.js
+++ src/components/ProfilePic/ProfilePic.js
@@ -17,40 +17,13 @@ export default () => {
     }
   }
   return (
-    <div className="register_wrapper">
-      <div className="register_player_column_layout_one">
-        <div className="register_player_Twocolumn_layout_two">
           <form className="myForm">
-            <div className="formInstructionsDiv formElement">
-              {/* <h2 className="formTitle">Sign Up</h2> */}
-              <p className="instructionsText" />
               <div className="register_profile_image">
                 <input id="profilePic" type="file" onChange={onChangePicture} />
               </div>
               <div className="previewProfilePic">
                 <img className="playerProfilePic_home_tile" src={imgData} width={100} height={100} />
               </div>
-            </div>
-            <div className="fillContentDiv formElement">
-              {/* <div className="names formContentElement">
-                <input
-                  className="inputRequest "
-                  type="text"
-                  placeholder="First Name"
-                />
-                <input
-                  className="inputRequest "
-                  type="text"
-                  placeholder="Last Name"
-                />
-              </div> */}
-            </div>
-            <div className="submitButtonDiv formElement">
-              {/* <button className="submitButton">Register</button> */}
-            </div>
           </form>
-        </div>
-      </div>
-    </div>
   );
 };
