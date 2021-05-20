c9e714d939e41309417bf619e9c6438f23fcdf9f
Stacy Grenier <sngrenier@gmail.com>
2021-05-20 15:11:01 -0700

    build folder

--- server/index.js
+++ server/index.js
@@ -124,8 +124,8 @@ app.get(`/getpicture`, profileCtrl.getPicture)
 
 
 
-app.use( express.static( `${__dirname}/../build`));
+app.use(express.static(`${__dirname}/../build`));
 
-app.get('*', (req,res)=> {
+app.get('*', (req,res) => {
 res.sendFile(path.join(__dirname, '../build/index.html'))
 })
